5f889911f7f3ee08d8d05eca14e1f8ba5daf2bad
Nika <nikarasevas@gmail.com>
2022-10-15 22:55:09 +0300

    Заполнение матрицы

--- Exercise_04/Program.cs
+++ Exercise_04/Program.cs
@@ -8,77 +8,102 @@
 26(1,0,1) 55(1,1,1) */
 
 
-int[,,] array = GetArray(2, 2, 2, 1, 10); 
-PrintArray(array);  
+
+int[] arr = GetArray1(1, 10, 99);
+Console.WriteLine($"{String.Join(",", arr)}");
 Console.WriteLine();
 
-int[,,] NewArray = CheckDublicate(array, 1, 10);
-PrintArray(NewArray);
 
-
-int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
+int[] GetArray1(int minValue, int maxvalue, int size)
 {
-    int[,,] result = new int[x, y, z];
-    for(int i = 0; i < x; i++)
+    int[] array1 = new int[size];
+    for (int i = 0; i < size; i++)
     {
-        for(int j = 0; j < y; j++)
+        bool isUnique;
+        do
         {
-            for(int k = 0; k < z; k++)
-            {
-                result[i, j, k] = new Random().Next(minValue, maxValue + 1);
-            }
-        }
-    }
-    return result;
-}
-
-void PrintArray(int[,,] array)
-{
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            for(int k = 0; k < array.GetLength(2); k++)
-            {
-                if (k == 0)
+            array1[i] = new Random().Next(minValue, maxvalue + 1);
+            isUnique = true;
+            for (int j = 0; j < i; j++)
+                if (array1[i] == array1[j])
                 {
-                    Console.Write($"{array[i, j, k]} {(i, j, k)} ");
+                    isUnique = false;
+                    break;
                 }
-                else
-                {
-                    Console.WriteLine($"{array[i, j, k]} {(i, j, k)} ");
-                }
-            }
-        }
+        } while (!isUnique);
     }
+    return array1;
 }
 
 
-int[,,] CheckDublicate(int[,,] array, int minValue, int maxValue)
-{
-    for(int x = array.GetLength(0); x > 0; x--)
-    {
-        for(int i = 0; i < array.GetLength(0) - 1; i++)
-        {
-            for(int y = array.GetLength(1); y > 0; y--)
-            {
-                for(int j = 0; j < array.GetLength(1) - 1; j++)
-                {
-                    for(int z = array.GetLength(2); z > 0; z--)
-                    {
-                        for(int k = 0; k < array.GetLength(2) - 1; k++)
-                        {
-                            if(array[i, j, k] == array[i, j, k + 1])
-                            {
-                                array[i, j, k + 1] = new Random().Next(minValue, maxValue + 1);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-    }
-    return array;
-}
+
+
+
+
+
+
+
+// int[,,] array = GetArray(2, 2, 2, 1, 10); 
+// PrintArray(array);  
+// Console.WriteLine();
+
+// int[,,] NewArray = CheckDublicate(array, 1, 10);
+// PrintArray(NewArray);
+
+
+// int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
+// {
+//     int[,,] result = new int[x, y, z];
+//     for(int i = 0; i < x; i++)
+//     {
+//         for(int j = 0; j < y; j++)
+//         {
+//             for(int k = 0; k < z; k++)
+//             {
+//                 result[i, j, k] = new Random().Next(minValue, maxValue + 1);
+//             }
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for(int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 if (k == 0)
+//                 {
+//                     Console.Write($"{array[i, j, k]} {(i, j, k)} ");
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"{array[i, j, k]} {(i, j, k)} ");
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+// int[,,] CheckDublicate(int[,,] array, int minValue, int maxValue)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 for(int k = 0; k < array.GetLength(2) - 1; k++)
+//                     {
+//                         if(array[i, j, k] == array[i, j, k + 1])
+//                         {
+//                             array[i, j, k + 1] = new Random().Next(minValue, maxValue + 1);
+//                         }
+//                     }
+//             }
+//     }
+//     return array;     
+// }
    

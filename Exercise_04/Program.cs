6aeaad07ba4bc75f1a910ab429189c554e205f5e
Nika <nikarasevas@gmail.com>
2022-10-15 23:55:32 +0300

    Трёхмерный массив из неповторяющихся элементов 2х2х2

--- Exercise_04/Program.cs
+++ Exercise_04/Program.cs
@@ -9,23 +9,25 @@
 
 
 
-int[] arr = GetArray1(1, 10, 99);
-Console.WriteLine($"{String.Join(",", arr)}");
-Console.WriteLine();
+int[] arr = GetArray1(10, 99, 8);
 
 
-int[] GetArray1(int minValue, int maxvalue, int size)
+int[,,] array3 = NewArray3(2, 2, 2, arr); 
+PrintArray(array3); 
+
+
+int[] GetArray1(int minValue, int maxValue, int size)
 {
     int[] array1 = new int[size];
-    for (int i = 0; i < size; i++)
+    for (int h = 0; h < size; h++)
     {
         bool isUnique;
         do
         {
-            array1[i] = new Random().Next(minValue, maxvalue + 1);
+            array1[h] = new Random().Next(minValue, maxValue + 1);
             isUnique = true;
-            for (int j = 0; j < i; j++)
-                if (array1[i] == array1[j])
+            for (int j = 0; j < h; j++)
+                if (array1[h] == array1[j])
                 {
                     isUnique = false;
                     break;
@@ -35,75 +37,45 @@ int[] GetArray1(int minValue, int maxvalue, int size)
     return array1;
 }
 
-
-
-
-
-
-
-
-
-// int[,,] array = GetArray(2, 2, 2, 1, 10); 
-// PrintArray(array);  
-// Console.WriteLine();
-
-// int[,,] NewArray = CheckDublicate(array, 1, 10);
-// PrintArray(NewArray);
-
-
-// int[,,] GetArray(int x, int y, int z, int minValue, int maxValue)
-// {
-//     int[,,] result = new int[x, y, z];
-//     for(int i = 0; i < x; i++)
-//     {
-//         for(int j = 0; j < y; j++)
-//         {
-//             for(int k = 0; k < z; k++)
-//             {
-//                 result[i, j, k] = new Random().Next(minValue, maxValue + 1);
-//             }
-//         }
-//     }
-//     return result;
-// }
-
-// void PrintArray(int[,,] array)
-// {
-//     for(int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for(int j = 0; j < array.GetLength(1); j++)
-//         {
-//             for(int k = 0; k < array.GetLength(2); k++)
-//             {
-//                 if (k == 0)
-//                 {
-//                     Console.Write($"{array[i, j, k]} {(i, j, k)} ");
-//                 }
-//                 else
-//                 {
-//                     Console.WriteLine($"{array[i, j, k]} {(i, j, k)} ");
-//                 }
-//             }
-//         }
-//     }
-// }
-
-
-// int[,,] CheckDublicate(int[,,] array, int minValue, int maxValue)
-// {
-//     for(int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for(int j = 0; j < array.GetLength(1); j++)
-//             {
-//                 for(int k = 0; k < array.GetLength(2) - 1; k++)
-//                     {
-//                         if(array[i, j, k] == array[i, j, k + 1])
-//                         {
-//                             array[i, j, k + 1] = new Random().Next(minValue, maxValue + 1);
-//                         }
-//                     }
-//             }
-//     }
-//     return array;     
-// }
-   
+int [,,] NewArray3(int x, int y, int z, int[] arr)
+{
+    int[,,] result = new int [x, y, z];
+    int h = 0;
+    int lenght = arr.Length;
+    while(h < lenght)
+    {
+        for(int i = 0; i < x; i++)
+        {
+            for(int j = 0; j < y; j++)
+            {
+                for(int k = 0; k < z; k++)
+                {
+                    result[i, j, k] = arr[h];
+                    h++;
+                }
+            }               
+        }
+    }
+    return result;
+}
+    
+void PrintArray(int[,,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                if (k == 0)
+                {
+                    Console.Write($"{array[i, j, k]} {(i, j, k)} ");
+                }
+                else
+                {
+                    Console.WriteLine($"{array[i, j, k]} {(i, j, k)} ");
+                }
+            }
+        }
+    }
+}
